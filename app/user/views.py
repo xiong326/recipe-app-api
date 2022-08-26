87409b5ef03fda8d9b9ac26e7b7d3a8ca2e45e36
xiong326 <xiong73@wisc.edu>
2022-08-26 14:59:53 -0500

    Fixed bug in user/views.py

--- app/user/views.py
+++ app/user/views.py
@@ -26,7 +26,7 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    permission_class = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
