f0930fee57a98def6824e4dc7715ce0ddd9839ab
xiong326 <xiong73@wisc.edu>
2022-08-27 16:31:11 -0500

    Implement update and delete ingredient.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,7 +54,10 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
     """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
